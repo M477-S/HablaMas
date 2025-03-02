329c2386cfc6bba8781eee7047e73065935ceb7e
M477-S <matthew.stanciu@icloud.com>
2025-03-02 17:46:26 +0000

    Initial Commit

---
+++ HablaMas/HablaMasApp.swift
@@ -0,0 +1,17 @@
+//
+//  HablaMasApp.swift
+//  HablaMas
+//
+//  Created by Matthew Stanciu on 02/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct HablaMasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HablaMasTests/HablaMasTests.swift
@@ -0,0 +1,17 @@
+//
+//  HablaMasTests.swift
+//  HablaMasTests
+//
+//  Created by Matthew Stanciu on 02/03/2025.
+//
+
+import Testing
+@testable import HablaMas
+
+struct HablaMasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
